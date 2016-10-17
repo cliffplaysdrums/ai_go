6f3629a75e6f16d73bc8e404827d1191eb852e58
Cliff Chandler <cliffplaysdrums@gmail.com>
2016-10-17 16:28:40 -0400

    Modified place_piece to use captured_pieces, which in turn uses eval_piece. Added 'help' comments to new_game and place_piece.

--- new_game.m
+++ new_game.m
@@ -1,3 +1,6 @@
+% function board_state = new_game(size)
+%
+% returns a (size x size) board
 function board_state = new_game(size)
     board_state(1:size,1:size) = 'n';
 end
--- place_piece.m
+++ place_piece.m
@@ -1,11 +1,23 @@
+% function [board_state, placed] =
+%     place_piece(board_state, point, color)
+%
+% point should be defined as an array: [row column]
+% color should be 'b' or 'w'
+
 function [board_state, placed] = place_piece(board_state, point, color)
     placed = false;
     
-    % point array 1 is y-value.  2 is x-value.
-    if strcmpi(board_state(point(1),point(2)), 'n')
+    % point array 1 is y-value (row). 2 is x-value (column).
+    
+    % I've commented out the majority of the code here to test my group capture.
+    % The group capture appears to work (I've only run a handful of tests)
+    % For now, the player is allowed to perform suicide (a smart player would
+    % never do this anyway
+    
+    %if strcmpi(board_state(point(1),point(2)), 'n')
         placed = true;
         board_state(point(1),point(2)) = color;
-        
+     %{   
         left = [point(1), point(2)-1];
         board_state = check_capture(board_state,left,color);
         right = [point(1), point(2)+1];
@@ -15,6 +27,20 @@ function [board_state, placed] = place_piece(board_state, point, color)
         bottom = [point(1)+1, point(2)];
         board_state = check_capture(board_state,bottom,color);
     end
+    %}
+    
+    if strcmpi(color, 'w')
+        opponent = 'b';
+    else
+        opponent = 'w';
+    end
+    
+    capturedIndices = captured_pieces(board_state, color, opponent);
+    for i = 1:size(capturedIndices, 1)
+        captured = capturedIndices(i, :);
+        board_state(captured(1), captured(2)) = 'n';
+    end
+    
 end
 
 function board_state = check_taken(board_state, group)
